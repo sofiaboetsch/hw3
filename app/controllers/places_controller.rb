28678609cf7349521693bd1ceb51afeadd6441db
sofiaboetsch <sofia.boetsch@kellogg.northwestern.edu>
2023-02-19 19:55:40 +0000

    update 2

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,21 @@
 class PlacesController < ApplicationController
+def index
+    @places= Place.all
+end
+
+def new
+    @place= Place.new
+end
+
+def create
+    @place = Place.new
+    @place["name"] = params["place"]["name"]
+    @place ["date"] = params["place"]["date"]
+    @place ["notes"]= params["place"]["notes"]
+    @place.save
+    redirect_to "/places"
+end
+
+
+
 end
