cc182a4ab3037bcf2c3d8462236db25613a31e86
sofiaboetsch <sofia.boetsch@kellogg.northwestern.edu>
2023-02-19 23:07:55 +0000

    update 4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,20 @@
 class PostsController < ApplicationController
+    def index
+        @post= Post.all
+    end
+       
+     def new
+            @post= Post.new
+        end
+    
+    def create
+            @post = Post.new
+            @post ["title"] = params["post"]["title"]
+            @post ["description"] = params["post"]["description"]
+            @post.save
+            redirect_to "/posts" 
+    end
+    
+
+
 end
